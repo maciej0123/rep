2e75de205a5cef32339bc44fdc0ca20997a6cd7c
maciej0123 <02likings.belated@icloud.com>
2023-09-15 10:22:37 +0200

    Create zmienne.php

---
+++ zmienne.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    
+    <body>
+            <?php
+             $title='123'
+             echo $title;
+            ?>
+            <?php echo ('Tu funkcja echo'); ?><br>
+            <?php echo 'Tu funkcja echo'; ?><br>
+            <?php echo 'Tu' . ' funkcja' . ' echo'; ?><br>
+            <?php echo ('Tu' . ' funkcja' . ' echo'); ?><br><br>
+
+    </body>
+</html>
