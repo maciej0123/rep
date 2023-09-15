2e080de533b8d4d1e40a9f21cc3e06dce0b5b959
maciej0123 <02likings.belated@icloud.com>
2023-09-15 09:57:05 +0200

    Create syntax.php

---
+++ syntax.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    
+    <body>
+            <?php echo ('Tu funkcja echo'); ?><br>
+            <?php echo 'Tu funkcja echo'; ?><br>
+            <?php echo 'Tu' . ' funkcja' . ' echo'; ?><br>
+            <?php echo ('Tu' . ' funkcja' . ' echo'); ?><br><br>
+
+            <?php print ('Zgłasza się funkcja print'); ?><br>
+            <?php print 'Zgłasza' . ' się' . ' funkcja' . ' print'; ?><br><br>
+
+            <?='Funkcja 1'; ?><br>
+            <?='Funkcja' . ' 2'; ?>
+ 
+            <?php
+            // komentarz 1
+            /* komentarz 2 */
+            ?>
+
+    </body>
+</html>
